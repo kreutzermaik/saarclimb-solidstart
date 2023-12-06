c412c8287bb7d11038efb75cbd64d94103bf7d6d
Maik Kreutzer <s17ce7@umwelt-campus.de>
2023-12-06 13:24:29 +0100

    updated cypress performance test

--- src/components/features/ProgressCard.tsx
+++ src/components/features/ProgressCard.tsx
@@ -288,12 +288,11 @@ export default function ProgressCard() {
                             {(item) => {
                                 return (
                                     <For each={item.progress}>
-                                        {(item: ProgressItem) => {
-                                            console.log(item);
+                                        {(item: ProgressItem, index) => {
                                             return (
                                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                                     <td class="px-3 py-2 w-2/12">
-                                                        <span class="minus-button">
+                                                        <span id={`button-minus-${index}`} class="minus-button">
                                                             <Button
                                                                 text="-"
                                                                 type="secondary"
@@ -317,7 +316,7 @@ export default function ProgressCard() {
                                                             value={item.value}
                                                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 mr-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-14 lg:w-44"
                                                         />
-                                                        <span class="plus-button">
+                                                        <span id={`button-plus-${index()}`} class="plus-button">
                                                           <Button
                                                               text="+"
                                                               type="secondary"
