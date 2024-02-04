58ee37c36d818e365068ba32169c2b36f95951f7
Maik Kreutzer <s17ce7@umwelt-campus.de>
2024-02-04 10:51:59 +0100

    supabase dev env

--- supabase.ts
+++ supabase.ts
@@ -3,12 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 let supabaseUrl: string = "";
 let supabaseAnonKey: string = "";
 
-if (process.env.NODE_ENV === "development") {
+/*if (process.env.NODE_ENV === "development") {*/
     supabaseUrl = import.meta.env.VITE_DEV_SUPABASE_URL;
     supabaseAnonKey = import.meta.env.VITE_DEV_SUPABASE_ANON_KEY;
-} else {
+/*} else {
     supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
     supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-}
+}*/
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
